c0fcc05761f88857062b1e9a6ef9ffb145b1b8d3
Magistra <magistraagis@gmail.com>
2023-08-07 15:16:25 +0700

    first commmit

--- Heath+/ContentView.swift
+++ Heath+/ContentView.swift
@@ -9,13 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(spacing: 32){
+                headerView
+                taskView
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+    }
+}
+
+extension ContentView {
+    var headerView: some View{
+        HStack{
+            VStack(alignment: .leading){
+                Text("Today")
+                    .font(.largeTitle).bold()
+                Text("10 Agustus 2022")
+                    .foregroundColor(.secondary)
+            }
+            Spacer()
+            ZStack{
+                Circle()
+                    .foregroundColor(.blue)
+                    .frame(width: 48)
+                Image(systemName: "person.fill")
+                    .foregroundColor(.white)
+            }
+            
+        }
+    }
+}
+
+extension ContentView{
+    var taskView: some View {
+        VStack(alignment: .leading){
+            Text("Your task's")
+                .font(.title2).bold()
+            TaskComponent()
+                
+        }
     }
 }
 
---
+++ Heath+/component/TaskComponent.swift
@@ -0,0 +1,51 @@
+//
+//  TaskView.swift
+//  Heath+
+//
+//  Created by magistra aptam on 07/08/23.
+//
+
+import SwiftUI
+
+struct TaskComponent: View {
+    var body: some View {
+        VStack(spacing: 16){
+            HStack{
+                Image(systemName: "figure.run")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 24)
+                    .foregroundColor(.blue)
+                Text("Running")
+                    .font(.headline)
+                Spacer()
+                Image(systemName: "chevron.forward")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 12)
+                    .foregroundColor(.gray)
+            }
+            VStack(alignment: .leading){
+                Text("Today at")
+                    .bold()
+                Divider()
+                Text("10:00 AM")
+                Divider()
+            }
+            
+        }
+        .padding(.vertical, 16)
+        .padding()
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color.gray)
+        )
+        
+    }
+}
+
+struct TaskComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskComponent()
+    }
+}
