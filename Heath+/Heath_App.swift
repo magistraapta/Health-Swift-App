b1be7683025e7c652ed0f95e6d4c6b4d86f904b5
Magistra <magistraagis@gmail.com>
2023-08-07 14:35:09 +0700

    Initial Commit

---
+++ Heath+/Heath_App.swift
@@ -0,0 +1,17 @@
+//
+//  Heath_App.swift
+//  Heath+
+//
+//  Created by magistra aptam on 07/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Heath_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
